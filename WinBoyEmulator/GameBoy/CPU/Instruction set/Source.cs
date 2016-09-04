1900a043fd29abb9d9afbc6c477fa07c0936c49f
saku-kaarakainen <sakuba91@hotmail.com>
2016-09-04 20:17:50 +0300

    #2: Added source.

---
+++ WinBoyEmulator/GameBoy/CPU/Instruction set/Source.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinBoyEmulator.GameBoy.CPU.Instruction_set
+{
+    /// <summary>
+    /// This class is used to determinite source of opcode <para/>
+    /// Use this with Instruction.Source
+    /// </summary>
+    public static class Source
+    {
+        /// <summary>d8 means immediate 8-bit data.</summary>
+        public const string d8 = "d8";
+
+        /// <summary>d8 means immediate 8-bit data.</summary>
+        public const string d8 = "d8";
+    }
+}
