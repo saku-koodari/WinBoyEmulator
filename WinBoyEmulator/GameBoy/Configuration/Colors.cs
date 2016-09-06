cb80d6b54e213a907fc51e7f23b393192f475798
saku-kaarakainen <sakuba91@hotmail.com>
2016-09-06 23:39:45 +0300

    Added enum Color for Colors on Configuration

--- WinBoyEmulator/GameBoy/Configuration/Colors.cs
+++ WinBoyEmulator/GameBoy/Configuration/Colors.cs
@@ -8,8 +8,12 @@ namespace WinBoyEmulator.GameBoy.Configuration
 {
     public static class Colors
     {
-        public static int Count = Palette.Length;
-
-        public static byte[] Palette = new byte[4] { 255, 192, 96, 0 };
+        public static byte[] Palette = new byte[4] 
+        {
+            (byte)Color.White,
+            (byte)Color.Silver,
+            (byte)Color.Grey,
+            (byte)Color.Black
+        };
     }
 }
