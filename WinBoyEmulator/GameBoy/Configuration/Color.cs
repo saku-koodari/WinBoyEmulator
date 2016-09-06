afee469a51bc5bba3acbd53061c3e39a8dc2196a
saku-kaarakainen <sakuba91@hotmail.com>
2016-09-06 23:49:48 +0300

    #7: Readd Color.cs

---
+++ WinBoyEmulator/GameBoy/Configuration/Color.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinBoyEmulator.GameBoy.Configuration
+{
+    public static class Color
+    {
+        public const byte White = 255;
+        public const byte Silver = 192;
+        public const byte Grey = 96;
+        public const byte Black = 0;
+    }
+}
--- WinBoyEmulator/GameBoy/Configuration/Colors.cs
+++ WinBoyEmulator/GameBoy/Configuration/Colors.cs
@@ -4,12 +4,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using static WinBoyEmulator.GameBoy.Configuration.Color;
+
 namespace WinBoyEmulator.GameBoy.Configuration
 {
     public static class Colors
     {
         public static int Count = Palette.Length;
 
-        public static byte[] Palette = new byte[4] { 255, 192, 96, 0 };
+        public static byte[] Palette = new byte[4] { White, Silver, Grey, Black };
     }
 }
