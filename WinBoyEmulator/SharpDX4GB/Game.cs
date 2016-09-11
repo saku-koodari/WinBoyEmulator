50d5997fff89de6c1c5b7732fb2a250bdabe35bf
saku-kaarakainen <sakuba91@hotmail.com>
2016-09-11 18:37:18 +0300

    #48: Initialize commit for drawing a triangle

--- WinBoyEmulator/SharpDX4GB/Game.cs
+++ WinBoyEmulator/SharpDX4GB/Game.cs
@@ -22,6 +22,7 @@ using System.Threading.Tasks;
 
 // Third party
 using SharpDX;
+using SharpDX.D3DCompiler;
 using SharpDX.Direct3D;
 using SharpDX.DXGI;
 using SharpDX.Windows;
@@ -37,21 +38,28 @@ namespace WinBoyEmulator.SharpDX4GB
 {
     public class Game : IDisposable
     {
-        public const int FPS = 120;
-        public const int Width = GameBoy.Configuration.Screen.Width;
-        public const int Height = GameBoy.Configuration.Screen.Height;
+        private const int FPS = 120;
+        private const int Width = GameBoy.Configuration.Screen.Width;
+        private const int Height = GameBoy.Configuration.Screen.Height;
+        private const string VertexShaderFile = "SharpDX4GB/vertexShader.hlsl";
+        private const string PixelShaderFile = "SharpDX4GB/pixelShader.hlsl";
 
         private LogWriter _logWriter;
         private Emulator _emulator;
 
-        private RenderForm _renderForm;
-        private Viewport _viewport;
         private ModeDescription _backBufferDescription;
+        private RenderForm _renderForm;
         private SwapChain _swapChain;
         private SwapChainDescription _swapChainDescription;
+        private Vector3[] _vertices;
+        private Viewport _viewport;
+        private D3D11.Buffer _triangleVertexBuffer;
         private D3D11.Device _d3dDevice;
         private D3D11.DeviceContext _d3dDeviceContext;
+        private D3D11.InputElement[] _inputElements;
+        private D3D11.PixelShader _pixelShader;
         private D3D11.RenderTargetView _renderTargetView;
+        private D3D11.VertexShader _vertexShader;
 
         /// <summary>Path of the game.</summary>
         public string GamePath
@@ -79,8 +87,16 @@ namespace WinBoyEmulator.SharpDX4GB
             _renderForm.AllowUserResizing = false;
 
             _initializeDeviceResources();
+            //_initializeShaders();
+            _initializeTriangle();
+
+            _inputElements = new D3D11.InputElement[]
+            {
+                new D3D11.InputElement("POSITION", 0, Format.R32G32B32A32_Float, 0)
+            };
         }
 
+        #region Initialize methods
         private void _initializeDeviceResources()
         {
             _backBufferDescription = new ModeDescription(Width, Height, new Rational(FPS, 1), Format.R8G8B8A8_UNorm);
@@ -106,6 +122,36 @@ namespace WinBoyEmulator.SharpDX4GB
             }
         }
 
+        private void _initializeShaders()
+        {
+            using (var vertexShaderByteCode = ShaderBytecode.CompileFromFile(VertexShaderFile, "main", "vs_4_0", ShaderFlags.Debug))
+            {
+                _vertexShader = new D3D11.VertexShader(_d3dDevice, vertexShaderByteCode);
+            }
+
+            using (var pixelShaderByteCode = ShaderBytecode.CompileFromFile(PixelShaderFile, "main", "ps_4_0", ShaderFlags.Debug))
+            {
+                _pixelShader = new D3D11.PixelShader(_d3dDevice, pixelShaderByteCode);
+            }
+
+            _d3dDeviceContext.VertexShader.Set(_vertexShader);
+            _d3dDeviceContext.PixelShader.Set(_pixelShader);
+
+            _d3dDeviceContext.InputAssembler.PrimitiveTopology = PrimitiveTopology.TriangleList;
+        }
+
+        private void _initializeTriangle()
+        {
+            _vertices = new Vector3[]
+            {
+                new Vector3(-0.5f, 0.5f, 0.0f),
+                new Vector3(0.5f, 0.5f, 0.0f),
+                new Vector3(0.0f, -0.5f, 0.0f)
+            };
+            _triangleVertexBuffer = D3D11.Buffer.Create(_d3dDevice, D3D11.BindFlags.VertexBuffer, _vertices);
+        }
+        #endregion
+
         private void _draw()
         {
             _d3dDeviceContext.OutputMerger.SetRenderTargets(_renderTargetView);
@@ -114,6 +160,37 @@ namespace WinBoyEmulator.SharpDX4GB
         }
 
         private void _renderCallBack() => _draw();
+
+        /// <summary>
+        /// Check if object is disposed. If it's not, then dispose the object. Else do nothing.
+        /// </summary>
+        /// <param name="disposableObject">object that inherit's SharpDX.DisposeBase</param>
+        private void _disposeIfNotDisposed(DisposeBase disposableObject, string nameofObject = null)
+        {
+            if (string.IsNullOrEmpty(nameofObject))
+                nameofObject = nameof(disposableObject);
+
+            if (!disposableObject.IsDisposed)
+            {
+                disposableObject.Dispose();
+                // Would it better to use trace? This might put too much data to output.
+                _logWriter.Debug($"Object: {nameofObject} disposed.");
+            }
+            else
+            {
+                var stackTrace = new StackTrace();
+                var stackFrame = stackTrace.GetFrame(1);
+                var method = stackFrame.GetMethod();
+                var message = $"Object: {nameofObject} is already disposed.\nSmall stack trace:\n";
+
+                for (var i = 0; i < 3; i++)
+                {
+                    message += $"i:{i}\tmethod{stackTrace.GetFrame(i).GetMethod()}\n";
+                }
+
+                _logWriter.Info(message);
+            }
+        }
 
         /// <summary>Runs a game.</summary>
         public void Run()
@@ -126,42 +203,14 @@ namespace WinBoyEmulator.SharpDX4GB
             RenderLoop.Run(_renderForm, _renderCallBack);
         }
 
-        /// <summary>
-        /// Check if object is disposed. If it's not, then dispose the object. Else do nothing.
-        /// </summary>
-        /// <param name="disposableObject">object that inherit's SharpDX.DisposeBase</param>
-        private void _disposeIfNotDisposed(DisposeBase disposableObject, string nameofObject = null)
-        {
-            if (string.IsNullOrEmpty(nameofObject))
-                nameofObject = nameof(disposableObject);
-
-            if ( !disposableObject.IsDisposed )
-            {
-                disposableObject.Dispose();
-                // Would it better to use trace? This might put too much data to output.
-                _logWriter.Debug($"Object: {nameofObject} disposed.");
-            }
-            else 
-            {
-                var stackTrace = new StackTrace();
-                var stackFrame = stackTrace.GetFrame(1);
-                var method = stackFrame.GetMethod();
-                var message = $"Object: {nameofObject} is already disposed.\nSmall stack trace:\n";
-
-                for(var i = 0; i < 3; i++)
-                {
-                    message += $"i:{i}\tmethod{stackTrace.GetFrame(i).GetMethod()}\n";
-                }
-
-                _logWriter.Info(message);
-            }
-        }
-
         /// <summary>Dispose objects.</summary>
         public void Dispose()
         {
             _emulator.StopEmulation();
 
+            _disposeIfNotDisposed(_triangleVertexBuffer, nameof(_triangleVertexBuffer));
+            _disposeIfNotDisposed(_vertexShader, nameof(_vertexShader));
+            _disposeIfNotDisposed(_pixelShader, nameof(_pixelShader));
             _disposeIfNotDisposed(_renderTargetView, nameof(_renderTargetView));
             _disposeIfNotDisposed(_swapChain, nameof(_renderTargetView));
             _disposeIfNotDisposed(_d3dDevice, nameof(_d3dDevice));
