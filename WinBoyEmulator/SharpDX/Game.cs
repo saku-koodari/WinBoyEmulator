8b222e02413e74cb4d3bb1c6bab4ec11ca9dcce6
saku-kaarakainen <sakuba91@hotmail.com>
2016-09-11 10:31:34 +0300

    #47: Downgraded libraries

--- WinBoyEmulator/SharpDX/Game.cs
+++ WinBoyEmulator/SharpDX/Game.cs
@@ -4,9 +4,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+//using SharpDX.Win32;
+
 namespace WinBoyEmulator.SharpDX
 {
     public class Game
     {
+        public Game()
+        {
+            var renderForm = new RenderForm("s");
+        }
     }
 }
